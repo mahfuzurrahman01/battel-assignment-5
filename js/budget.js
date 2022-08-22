9fb8970bd2efef6ab51dedb21d8264affc89ddc4
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-08-22 16:47:29 +0600

    almost done

--- js/budget.js
+++ js/budget.js
@@ -1,23 +1,22 @@
 // calculate button click event start here
 document.getElementById('calculate').addEventListener('click', function () {
-    // get the field value with common funtion from utilities.js file
+    // get the budget field value with common funtion from utilities.js file
     const budgetFieldValue = inputValue('per-player-budget');
     // condition for empty field 
-    if (isNaN(budgetFieldValue)) {
+    if (isNaN(budgetFieldValue) || budgetFieldValue < 0) {
         alert('Please input valid digits only!')
         //reset the field
         const budgetField = getTheField('per-player-budget');
         budgetField.value = '';
         return;
     }
-    const ManagerFieldValue = inputValue('manager-budget');
-    const coachFieldValue = inputValue('coach-budget');
+
     // get the order list with common funtion using from utilities file
     const orderlist = getTheElement('order-list');
     //length-1 is using for 
     const orderlistItems = orderlist.childNodes.length - 1;
     //condition for empty selection
-    if(orderlist.childNodes.length-1 === 0){
+    if (orderlist.childNodes.length - 1 === 0) {
         alert('First you have to select players!');
         return;
     }
@@ -25,4 +24,21 @@ document.getElementById('calculate').addEventListener('click', function () {
     const playerExpenses = budgetFieldValue * orderlistItems;
     //set the playerExpenses value to the player expanse field;
     updateInnerText('player-expenses', playerExpenses);
+})
+// total calculation button event start here 
+document.getElementById('calculate-total').addEventListener('click', function () {
+    // get the field value of manager and coach 
+    const ManagerFieldValue = inputValue('manager-budget');
+    const coachFieldValue = inputValue('coach-budget');
+    // condition for empty field 
+    if (isNaN(ManagerFieldValue) || isNaN(coachFieldValue) || ManagerFieldValue < 0 || coachFieldValue < 0) {
+        alert('Input error : input should be in digits and not should be a negetive number!');
+        return;
+    }
+    // get the inner text of player expenses 
+    const playerExpensesInnerText = getInnerText('player-expenses');
+    // sum of total cost 
+    const totalExpenses = ManagerFieldValue + coachFieldValue + playerExpensesInnerText;
+    //set the value to the total field
+    updateInnerText('total-expense', totalExpenses);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,22 +1,28 @@
 //funtion for: get Element by id function 
-function getTheElement (elementId){
+function getTheElement(elementId) {
     const element = document.getElementById(elementId);
     return element;
 
 }
 //funtion for: get the input value funtion
-function inputValue(elementId){
+function inputValue(elementId) {
     const field = document.getElementById(elementId);
     const fieldValue = parseFloat(field.value);
     return fieldValue;
 }
 //funtion for: set the innerText 
-function updateInnerText(elementId,value){
+function updateInnerText(elementId, value) {
     const element = document.getElementById(elementId);
     element.innerText = value;
 }
 //function for: only getting the field
-function getTheField(elementId){
+function getTheField(elementId) {
     const field = document.getElementById(elementId);
     return field;
+}
+//funtion for getting innertext
+function getInnerText(elementId) {
+    const element = document.getElementById(elementId);
+    const elementInnerText = parseFloat(element.innerText);
+    return elementInnerText;
 }
